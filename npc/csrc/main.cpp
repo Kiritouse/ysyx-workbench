25a0262b3f3db6534496b49e4b8cdec9e5306641
Kiritouse <2496305505@qq.com>
2024-10-28 17:02:55 +0800

    搭建了nvboard和verilator和基本环境

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,32 +1,43 @@
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <verilated.h>
-#include <verilated_fst_c.h>
+ 
 #include "Vtop.h"
-int main(int argc,char** argv){
-
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vtop* top = new Vtop(contextp);//实例化模块
-    VerilatedFstC* tfp = new VerilatedFstC;//初始化波形文件
-    contextp->traceEverOn(true);//追踪波形
-    top->trace(tfp, 99);//追踪波形
-    tfp->open("simx.fst");//打开波形文件
-    while (!contextp->gotFinish()) {
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval(); //验证模型
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        tfp->dump(contextp->time()); //波形文件,dump wave
-        contextp->timeInc(1); //时间步进
-        assert(top->f == (a ^ b));
-    }
-    top->final(); //模型销毁
-    delete top;
-    tfp->close();
-    delete contextp;
-    return 0;
+#include "verilated.h"
+ 
+#include "verilated_vcd_c.h"
+ 
+int main(int argc,char** argv,char** env){
+	  VerilatedContext* contextp = new VerilatedContext;
+		contextp->commandArgs(argc, argv);
+		Vtop* top = new Vtop{contextp};
+ 
+		VerilatedVcdC* tfp = new VerilatedVcdC;//初始化VCD对象指针
+		contextp->traceEverOn(true);//打开追踪功能
+		top->trace(tfp,0);
+		tfp->open("wave.vcd");//设置输出的文件wave.vcd
+ 
+ 
+		while(!contextp->gotFinish())
+		{
+			for(int i = 0;i < 10; i++) {
+				int a = rand() & 1;
+				int b = rand() & 1;
+				top->a = a;
+				top->b = b;
+				top->eval();
+				printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+ 
+				tfp->dump(contextp->time());//dump wave
+				contextp->timeInc(1);//推动仿真时间
+				
+				assert(top->f == (a ^ b));
+			}
+//			$finish;
+	}
+		delete top;
+		tfp->close();
+		delete contextp;
+		return 0;	
 }

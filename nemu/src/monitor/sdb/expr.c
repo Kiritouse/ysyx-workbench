824dc0886cce936b66fb922611ee64510a4b0e56
tracer-ysyx <tracer@ysyx.org>
2024-11-03 19:07:21 +0800

    > compile NEMU ysyx_22040000 吴畅 Linux DESKTOP-PC75N70 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:07:21 up 37 min,  1 user,  load average: 0.34, 0.21, 0.12

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -22,14 +22,15 @@
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
-
+  TK_NUM,
+  TK_LEFT_BRACKET,TK_RIGHT_BRACKET,
   /* TODO: Add more token types */
 
 };
 
 static struct rule {
-  const char *regex;
-  int token_type;
+  const char *regex; //正则表达式
+  int token_type;//token类型
 } rules[] = {
 
   /* TODO: Add more rules.
@@ -38,8 +39,15 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
+  {"\\-", '-'},         // sub
+  {"\\*", '*'},         // mul
+  {"\\/", '/'},         // div
+  {"\\(",TK_LEFT_BRACKET},//左括号
+  {"\\)",TK_RIGHT_BRACKET},//右括号
+  {"[0-9]+", TK_NUM},//num
   {"==", TK_EQ},        // equal
 };
+
 
 #define NR_REGEX ARRLEN(rules)
 
@@ -80,10 +88,11 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+      //按顺序尝试用不同的规则来匹配当前位置的字符串. 当一条规则匹配成功, 并且匹配出的子串正好是position所在位置的时候, 我们就成功地识别出一个token
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) { //position为当前处理到的位置
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-
+        assert(substr_len<32);
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
@@ -93,8 +102,35 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
-        switch (rules[i].token_type) {
+        switch (rules[i].token_type) { //
+          case TK_NOTYPE: //遇到空格即使匹配上了也不用管
+            break;
+          case '+':
+            tokens[nr_token++].type= '+';
+            break;
+          case '-':
+            tokens[nr_token++].type = '-';
+            break;
+          case '*':
+            tokens[nr_token++].type = '*';
+            break;
+          case '/':
+            tokens[nr_token++].type = '/';
+            break;
+          case TK_LEFT_BRACKET:
+            tokens[nr_token++].type = '(';
+            break;
+          case TK_RIGHT_BRACKET:
+            tokens[nr_token++].type = ')';
+            break;
+          
+          //need to record sub_str
+          case TK_EQ:
+            tokens[nr_token].type = TK_EQ;
+            strncpy(tokens[nr_token++].str, substr_start,substr_len);
+          case TK_NUM:
+            tokens[nr_token].type = TK_NUM;
+            strncpy(tokens[nr_token++].str, substr_start,substr_len);
           default: TODO();
         }
 
@@ -111,7 +147,66 @@ static bool make_token(char *e) {
   return true;
 }
 
+uint32_t find_op(uint32_t p,uint32_t q){
+  uint32_t min_op = -1;//记录优先级最低的符号
+  for(uint32_t i = p;i<=q;i++){
+    if(tokens[i].type=='('){ //main op 一定不在括号的表达式中
+      while(tokens[i].type!=')')i++;
+    }
+    if(tokens[i].type=='*'||tokens[i].type=='/'){
+        if(min_op==-1) min_op = i;
+        if(tokens[min_op].type=='*'||tokens[min_op].type=='/'){// 查看上一个op的符号类型
+          min_op = i;
+        }        
+    }
+    if(tokens[i].type=='+'||tokens[i].type=='-'){
+      min_op = i;
+    }
+  }
+  return min_op;
+}
+bool check_parentheses(int p,int q){ //检查p和q包围的表达式是否
 
+  return false;
+}
+word_t eval(uint32_t p,uint32_t q){  //p,q指示表达式的开始位置和结束位置
+  if(p>q){
+    assert(0);
+    return -1;
+  }
+  else if(p==q){
+    return atoi(tokens[p].str);
+  }
+  else if(check_parentheses(p,q)){ //如果p,q被对配对的括号包围
+    return eval(p+1,q-1);
+  }
+  else{
+    uint32_t op = find_op(p,q);
+    uint32_t left_ans = eval(p,op-1);
+    uint32_t right_ans = eval(op+1,q);
+
+    switch(tokens[op].type){
+      case '+':
+        return left_ans+right_ans;
+      break;
+      case '-':
+        return left_ans-right_ans;
+      break;
+      case '*':
+        return left_ans*right_ans;
+      break;
+      case '/':
+        assert(right_ans!=0);
+        return left_ans/right_ans;
+      break;
+      default:
+      assert(0);
+      break;
+    }
+    
+  }
+
+}
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -119,7 +214,5 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  return eval(0,nr_token-1);
 }

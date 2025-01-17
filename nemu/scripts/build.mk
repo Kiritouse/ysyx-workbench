d9436d5db5db1870f00441faa7f22afd04035062
tracer-ysyx <tracer@ysyx.org>
2025-01-17 21:11:48 +0800

    > compile NEMU ysyx_22040000 吴畅 Linux DESKTOP-PC75N70 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:11:48 up  6:12,  1 user,  load average: 0.16, 0.08, 0.04

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -62,3 +62,21 @@ count:
 	@echo "Total lines of code excluding empty lines in .c and .h files:"
 	@find ./ -name '*.c' -o -name '*.h' | xargs grep -v '^\s*$$' | wc -l
 	
+count_pa1:
+	@( \
+      git stash > /dev/null 2>&1; \
+      git checkout pa0 > /dev/null 2>&1; \
+      echo "[Before PA1]"; \
+      pa0_lines=$$(find ./ -name '*.c' -o -name '*.h' | xargs wc -l | tail -n 1 | awk '{print $$1}'); \
+      echo "  Lines of code before PA1: $$pa0_lines"; \
+      \
+      git checkout - > /dev/null 2>&1; \
+      git stash pop > /dev/null 2>&1; \
+      echo "[After PA1]"; \
+      pa1_lines=$$(find ./ -name '*.c' -o -name '*.h' | xargs wc -l | tail -n 1 | awk '{print $$1}'); \
+      echo "  Lines of code after PA1:  $$pa1_lines"; \
+      \
+      echo "[Diff]"; \
+      echo "  Lines of code written in PA1: $$((pa1_lines - pa0_lines))"; \
+    )
+	

cc98376e087166f5c7b14580a038848db40132c3
tracer-ysyx <tracer@ysyx.org>
2025-01-30 02:16:55 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  02:16:55 up 6 days, 23:44,  0 user,  load average: 0.37, 0.18, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,18 +34,20 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = SEXT(( \
-(BITS(i, 31, 31) << 19) | \
-BITS(i, 30, 21) | \
-(BITS(i, 20, 20) << 10) | \
-(BITS(i, 19, 12) << 11) \
-) << 1, 21);} while(0)
+(BITS(i, 31, 31) << 20) | \
+BITS(i, 30, 21) <<  1| \
+(BITS(i, 20, 20) << 11) | \
+(BITS(i, 19, 12) << 12) \
+), 21);} while(0)
 //这个jal的imm有点反尝试了，31-21位对应的是imm数的20，10:1 ，11，19:12 顺序非常的怪
-#define immB() do { *imm = (SEXT(( \
+/*
+#define immB() do { *imm = SEXT(( \
 (BITS(i, 31, 31) << 12) | \
 (BITS(i, 30, 25) << 5) | \
 (BITS(i, 11, 8) << 1) | \
 (BITS(i, 7, 7) << 11) \
-), 13) << 1); } while(0)
+)<<1,); } while(0)
+*/
 
 //这个函数是根据不同的指令类型来获取各个指令中各个寄存器所对应的域。用于更新寄存器
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -59,7 +61,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
     case TYPE_R: src1R(); src2R(); break;
-    case TYPE_B: src1R(); src2R(); immB(); break;
+    //case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }

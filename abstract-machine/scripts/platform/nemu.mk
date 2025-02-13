318eceb66f6dd18c0132cf1b59003f547425a81c
Kiritouse <2496305505@qq.com>
2025-02-13 13:36:48 +0000

    feat(nemu.mk)add args -b in NEMUFLAGS to enable batch mode

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,6 +13,8 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+# 添加 -b 参数，使得 NEMU 在运行时不会显示任何输出
+NEMUFLAGS += -b
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.

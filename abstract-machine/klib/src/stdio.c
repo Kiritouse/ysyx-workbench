ea4b9ac4ad9dcf4a961bf5d7e011ef97782efcd1
tracer-ysyx <tracer@ysyx.org>
2025-02-15 12:11:12 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  12:11:12 up 23 days,  9:38,  0 user,  load average: 0.08, 0.03, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,6 +4,33 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+static void reverse(char *s, int len) { 
+  char *end = s + len - 1;
+  char tmp;
+  while (s < end) {
+    tmp = *s;
+    *s = *end;
+    *end = tmp;
+  }
+}
+
+static int itoa(int n, char *s, int base) {
+  assert(base <= 16);
+
+  int i = 0, sign = n, bit;
+  if (sign < 0) n = -n;
+  do {
+    bit = n % base;
+    if (bit >= 10) s[i++] = 'a' + bit - 10;
+    else s[i++] = '0' + bit;
+  } while ((n /= base) > 0);
+  if (sign < 0) s[i++] = '-';
+  s[i] = '\0';
+  reverse(s, i);
+
+  return i;
+}
+
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
@@ -14,7 +41,29 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list _Args;
+  va_start(_Args,fmt);
+  char *start = out;
+  for(;*fmt!='\0';fmt++){
+    if(*fmt!='%'){
+      *out = *fmt;
+      out++;
+    }
+    else{
+      switch(*(++fmt)){ //看占位符%下一位是什么
+        case '%':*out = *fmt;out++;break;
+        case 'd': out += itoa(va_arg(_Args, int), out, 10); break;
+        case 's':
+          char *s = va_arg(_Args, char*);
+          strcpy(out, s);
+          out += strlen(out);
+          break;
+        }
+      }
+    }
+    *out = '\0';
+    va_end(_Args);  
+    return out - start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

9a72219369555a771027bd1d1c7d90fcf68cb771
tracer-ysyx <tracer@ysyx.org>
2025-02-22 14:16:11 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  14:16:11 up 30 days, 11:43,  0 user,  load average: 0.61, 0.29, 0.15

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -67,22 +67,52 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
   assert(s);
-  unsigned char *p = s;
+   char *p = (char*)s;
   while(n--) {
-    *p++ = (unsigned char)c;
+    *p++ = c;
   }
   return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char*p = dst;
-  while(n--){
-    *p++ = *(unsigned char*)src++;
-  }
-  return dst;
-}
+/*
+Copies the values of num bytes from the location pointed by source to the memory block pointed by destination. Copying takes place as if an intermediate buffer were used, allowing the destination and source to overlap.
+从source（源内存块位置）直接指向的地方开始复制num个字节的数据到destination指向的内存块位置。然后复制就像使用了中间缓冲区一样，允许destination和source重叠。
 
-//TODO:处理内存重叠和进行优化
+The function does not check for any terminating null character in source - it always copies exactly num bytes.
+这个函数不会检查'\0'，不会遇到'\0'就停下来，它就只认识要复制的num个字节数据。
+
+To avoid overflows, the size of the arrays pointed by both the destination and source parameters, shall be at least num bytes.
+为了避免溢出，这两个数组的大小至少为num个字节。 
+避免memcpy导致的重叠部分被覆盖的情况
+如
+*/
+void *memmove(void *dst, const void *src, size_t n) {
+	int i;
+	char *dest = (char *)dst;
+	char *source = (char *)src;
+	if(dst <= src){
+		for(i = 0;i < n;i++){
+			*dest++ = *source++;
+		}
+	}
+	else{ //从后往前
+		dest += n-1;
+		source += n-1;
+		for(i = 0;i < n;i++){
+			*dest-- = *source--;
+		}
+	}
+	return (void *)dst;
+}
+/*
+The function does not check for any terminating null character in source - it always copies exactly num bytes.
+
+这个函数不会检查'\0'，不会遇到'\0'就停下来，它就只认识要复制的num个字节数据。
+
+To avoid overflows, the size of the arrays pointed to by both the destination and source parameters, shall be at least num bytes, and should not overlap (for overlapping memory blocks, memmove is a safer approach).
+
+为了避免溢出，这两个数组的大小至少为num个字节，而且这两个数组内存位置不应该重叠。
+*/
 void *memcpy(void *out, const void *in, size_t n) {
   assert(out && in);
   char*pout = out;
@@ -94,9 +124,11 @@ void *memcpy(void *out, const void *in, size_t n) {
   
 }
 
+
+//
 int memcmp(const void *s1, const void *s2, size_t n) {
-  const unsigned char *ps1 = (const unsigned char *)s1;
-  const unsigned char *ps2 = (const unsigned char *)s2;
+  const  char *ps1 = (const char *)s1;
+  const char *ps2 = (const char *)s2;
   while (n--) {
     if (*ps1 != *ps2) {
     return *ps1 - *ps2;

cb764f2cdadf3f8823010b614edd4376dd7efcf1
tracer-ysyx <tracer@ysyx.org>
2025-03-01 14:04:33 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  14:04:33 up 37 days, 11:31,  0 user,  load average: 0.83, 0.34, 0.16

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -17,7 +17,7 @@
 #include <cpu/decode.h>
 #include <cpu/difftest.h>
 #include <locale.h>
-
+#include <utils.h>
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -39,8 +39,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-
   IFDEF(CONFIG_WATCHPOINT, difftest_wp());
+
+
+
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
@@ -51,7 +53,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   //log 追踪
 #ifdef CONFIG_ITRACE 
   char *p = s->logbuf;
-  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);//打印pc
+  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);//打印pc,最开始的地址
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst;
@@ -60,35 +62,46 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #else
   for (i = ilen - 1; i >= 0; i --) {
 #endif
-    p += snprintf(p, 4, " %02x", inst[i]);//16进制打印，不足2位前面补0，打印指令的16进制
+    p += snprintf(p, 4, " %02x", inst[i]);//指令16进制形式打印，不足2位前面补0，打印 *指令的16进制*
   }
+  //空格对齐
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len); //填充空格
   p += space_len;//移动到空格末尾
+  //空格对齐
 
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);//还有这种用法，只在一个函数中声明一个函数
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
-#endif //打印出反汇编指令的内容
+#endif //根据当前指令inst(2进制数据/16进制数据)进行反汇编，输出反汇编指令
 
-// //扫描所有监视点
-// #ifdef CONFIG_WATCHPOINT
-//   difftest_wp();
-// #endif
+//TODO:到时候将这一行p写入到iringbuffer即可
+//TODO:如何保证到时候读取的时候能够一行一行地读取？
+ring_buffer_write(s->logbuf, strlen(s->logbuf));
+
+
 }
 
 
 
 static void execute(uint64_t n) {
   Decode s;
+  unsigned char* r_ptr = NULL;
   for (;n > 0; n --) {
-    exec_once(&s, cpu.pc);
+    exec_once(&s, cpu.pc); //具体指令的执行，记录每次执行的log
     g_nr_guest_inst ++;
-    trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    trace_and_difftest(&s, cpu.pc);//写入Decode的logbuf 
+    if (nemu_state.state != NEMU_RUNNING){
+      //TODO:输出ring_buffer中的所有内容
+      //TODO:如何保证能够指令一行指令地读取
+      r_ptr = get_ring_buffer_read_pointer();
+      assert(r_ptr);
+      log_write("--->:%s\n",r_ptr); 
+      break;
+    }
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }

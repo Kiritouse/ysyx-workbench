0f00d5c2d3538b7a8163b12b7e01c2dca07e1c4d
tracer-ysyx <tracer@ysyx.org>
2025-03-11 02:26:45 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  02:26:45 up 46 days, 23:54,  0 user,  load average: 0.25, 0.17, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -37,6 +37,7 @@ void iringbuf_write(char*inst);
 void iringbuf_print();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+  IFDEF(CONFIG_ITRACE, iringbuf_write(_this->logbuf));
   iringbuf_write(_this->logbuf);
 
 #ifdef CONFIG_ITRACE_COND

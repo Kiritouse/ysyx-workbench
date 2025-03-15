94967ea51fc4ffd4dc410c44a4b9d0201104e39a
Kiritouse <2496305505@qq.com>
2025-03-15 06:32:15 +0000

    finish mtrace feat

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -25,13 +25,9 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
 
-void display_pread(paddr_t addr, int len) {
-  printf("pread at " FMT_PADDR " len=%d\n", addr, len);
-}
+void display_pread(paddr_t addr, int len);
+void display_pwrite(paddr_t addr, int len, word_t data);
 
-void display_pwrite(paddr_t addr, int len, word_t data) {
-  printf("pwrite at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
-}
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
@@ -61,7 +57,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  //IFDEF(CONFIG_MTRACE, display_pread(addr, len));
+  IFDEF(CONFIG_MTRACE, display_pread(addr, len));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -69,7 +65,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  //IFDEF(CONFIG_MTRACE, display_pwrite(addr, len,data));
+  IFDEF(CONFIG_MTRACE, display_pwrite(addr, len,data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);

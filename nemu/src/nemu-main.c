be2cb1fb292a43df2e57e938fe54d70af1371791
tracer-ysyx <tracer@ysyx.org>
2025-03-26 15:06:13 +0000

    > compile NEMU ysyx_25010015 吴畅 Linux instance-20250123-023128 6.1.0-29-cloud-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.123-1 (2025-01-02) x86_64 GNU/Linux  15:06:13 up 62 days, 12:33,  0 user,  load average: 0.55, 0.35, 0.17

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,8 +19,8 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
-void ring_buffer_destroy(IRingBuf_T* *rb);
-
+void iringbuf_free();
+extern IRingBuf_T *global_iring_buf;
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
@@ -31,7 +31,6 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();//包含主循环
-
-
+  iringbuf_free();
   return is_exit_status_bad();
 }
